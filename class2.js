1059b170410c2a09ec3bc0778ca96ec0e0d0d352
aditi saudagar <aditisaudagar17@gmail.com>
2024-09-18 12:23:21 +0530

    second commit

---
+++ class2.js
@@ -0,0 +1,18 @@
+function class2(){
+    // let a = parseInt(window.prompt("enter number1"));
+    // let b =parseInt(window.prompt("enter number2"));
+    // window.alert(a+b);
+    var a = "p35 js";
+    console.log(a);
+    var a = "25";
+    console.log(a);
+// re-declare and re-initialize
+    let b = "p35js";
+    b = 25;
+    console.log(b);
+//  only re-declare 
+    const c = "p35js";
+    console.log(c);
+    // not redeclare and not reinitialize
+}
+class2();
