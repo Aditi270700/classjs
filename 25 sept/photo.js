9589beb79b07db904fd6ca6597f16362e42b4445
aditi saudagar <aditisaudagar17@gmail.com>
2024-09-25 21:41:33 +0530

    commit

---
+++ 25 sept/photo.js
@@ -0,0 +1,9 @@
+function run(){
+    let selectphoto = document.querySelector('#photo');
+    selectphoto.src = "./annie.jpg"
+
+}
+function un(){
+    let selectdul = document.querySelector('ul');
+    selectdul.type = "circle";
+}
