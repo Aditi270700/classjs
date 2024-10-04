384d7de95678b6c1998dbda3185f45f97d295d28
aditi saudagar <aditisaudagar17@gmail.com>
2024-10-04 18:56:44 +0530

    commit

---
+++ 4 oct/time.js
@@ -0,0 +1,21 @@
+// setTimeout and setInterval
+// setTimeout is an higher order function 
+// setTimeout(run,3000);
+
+// function run(){
+// console.log("called")
+// }
+
+// setTimeout(()=>{
+// console.log("second time called");
+// },5000)
+function kuchbhi(){
+setTimeout(show,3000);
+function show(){
+let selectwebsite = document.querySelector('#website');
+selectwebsite.style.display = "block";
+
+let selectimg = document.querySelector('#img');
+selectimg.style.display = "none"
+    }
+}
