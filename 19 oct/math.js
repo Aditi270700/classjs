ec6ea90393ca0cc008d9ff1f2a436d497edff621
aditi saudagar <aditisaudagar17@gmail.com>
2024-10-19 21:15:33 +0530

    math

---
+++ 19 oct/math.js
@@ -0,0 +1,13 @@
+let a = Math.PI;
+console.log(a)
+let b = Math.SQRT;
+console.log(b)
+
+let c = 56.7;
+console.log(Math.round(c))
+console.log(Math.floor(c))
+console.log(Math.ceil(c))
+console.log(Math.trunc(c))
+console.log(Math.abs(c))
+console.log(Math.pow(2,3))
+console.log(Math.random())
---
+++ 19 oct/math1.js
@@ -0,0 +1,16 @@
+let selectbtn = document.querySelector('#btn');
+selectbtn.addEventListener('click',show);
+function show(){
+let str = '1234567980qwerrtouhjdhfjsdfljkfhvjlfdb^%$@&^%*&'
+let store = ''
+for(let i =0;i<6;i++){
+    let random = Math.random();
+     let ans = random*str.length;
+    let afterfloor = Math.floor(ans);
+     let cha = str.charAt(afterfloor);
+    store = store + cha
+}
+console.log(store);
+let selecth1 = document.querySelector('#output');
+selecth1.innerHTML = store
+}
