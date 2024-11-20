d2414ca33bc09bc651664629c2b8b259b70a7705
aditi saudagar <aditisaudagar17@gmail.com>
2024-11-20 12:26:21 +0530

    commit

--- 19 nov/index.js
+++ 19 nov/index.js
@@ -1,15 +1,38 @@
+// async function fet_data(){
+//     let res = await fetch("http://localhost:3000/students")
+//     let data = await res.json()
+//     let final_data = data.map((t)=>`
+//     <h1>${t.id}</h1>
+//     <h1>${t.name}</h1>
+//     <h1>${t.age}</h1>
+//     <h1>${t.address}</h1>
+//     `).join("")
+//     document.querySelector('#showdata').innerHTML=final_data
+// }
+// fet_data()
+
 async function fet_data(){
     let res = await fetch("http://localhost:3000/students")
     let data = await res.json()
     let final_data = data.map((t)=>`
-    <h1>${t.id}</h1>
-    <h1>${t.name}</h1>
-    <h1>${t.age}</h1>
-    <h1>${t.address}</h1>
+    <tr>
+    <td>${t.id}</td>
+    <td>${t.name}</td>
+    <td>${t.age}</td>
+    <td>${t.address}</td>
+    <td><button onclick="mydelete('${t.id}')">Delete</button></td>
+    </tr>
     `).join("")
 
     document.querySelector('#showdata').innerHTML=final_data
 }
 fet_data()
+
+function mydelete(id){
+    fetch(`http://localhost:3000/students/${id}`,{
+        method:'DELETE'
+    })
+}
+
 // map method is used to fetch data
 // node js is run time enviornment
